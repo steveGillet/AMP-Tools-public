924bbeda418c0a67e122583b99ffbf5804450bb7
steveGillet <stephen.gillet@gmail.edu>
2024-10-08 20:35:09 -0600

    finished hw 5

--- ws/hw5/MyGDAlgorithm.cpp
+++ ws/hw5/MyGDAlgorithm.cpp
@@ -1,7 +1,6 @@
 #include "MyGDAlgorithm.h"
 #include <cmath>
 
-// Implement the plan method for the gradient descent algorithm.
 amp::Path2D MyGDAlgorithm::plan(const amp::Problem2D& problem) {
     amp::Path2D path;
 
